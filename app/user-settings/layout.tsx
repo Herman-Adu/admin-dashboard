b29ed3579f67ca9815d8826dbb353d73d2408e69
Herman Adu <herman.adu@hotmail.com>
2025-05-12 20:43:34 +0100

    added advance user settings block to dashboard

--- app/user-settings/layout.tsx
+++ app/user-settings/layout.tsx
@@ -56,7 +56,7 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
           className="hidden dark:block"
         />
       </div>
-      <div className="hidden space-y-6 p-10 pb-16 md:block">
+      <div className="hidden space-y-6 p-10 pb-16 md:block ">
         <div className="space-y-0.5">
           <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
           <p className="text-muted-foreground">
@@ -65,10 +65,10 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
         </div>
         <Separator className="my-6" />
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/5">
+          <aside className="-mx-4 w-3/12">
             <SidebarNav items={sidebarNavItems} />
           </aside>
-          <div className="flex-1 lg:max-w-2xl ml-16">{children}</div>
+          <div className="flex-1w-9/12 ">{children}</div>
           <Toaster />
         </div>
       </div>
