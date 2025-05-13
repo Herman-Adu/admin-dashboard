3073ba01f6d1e1163890dbf1642d8a0ec31152e0
Herman Adu <herman.adu@hotmail.com>
2025-05-13 20:50:22 +0100

    fixed mobile view transactions lists

--- app/financial-dashboard/transactions/layout.tsx
+++ app/financial-dashboard/transactions/layout.tsx
@@ -17,18 +17,8 @@ export default function TransactionsLayout({
 }: TransactionsLayoutProps) {
   return (
     <>
-      {/* <div className="hidden space-y-6  pb-16 md:block "> */}
-      <div className="w-full px-10 justify-start items-start gap-8">
-        <div className="space-y-0.5">
-          <h2 className="text-2xl font-bold tracking-tight">Transactions</h2>
-          <p className="text-muted-foreground">
-            Here&apos;s a list of your transaction over the last month!
-          </p>
-        </div>
-        <Separator className="my-6" />
-        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0 w-full">
-          {children}
-        </div>
+      <div className="flex flex-col  lg:flex-row lg:space-x-12 lg:space-y-0 w-full">
+        {children}
       </div>
     </>
   );
